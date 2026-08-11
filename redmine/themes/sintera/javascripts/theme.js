22241036dc42dd892d973e9538dca72d7cd1e43d
Federico Ferreyra <federicoferreyrab@gmail.com>
2026-08-10 21:35:17 -0300

    Primera edicion de tema

---
+++ redmine/themes/sintera/javascripts/theme.js
@@ -0,0 +1,10 @@
+/*
+ * Sintera Theme
+ *
+ * JavaScript entry point.
+ *
+ * Keep this file intentionally minimal during the
+ * initial visual implementation.
+ */
+
+document.documentElement.classList.add("sintera");
